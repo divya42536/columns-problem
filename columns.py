ea33af96169a01da9676a6144dc07be8c7cfa049
divya42536 <divya42536@gmail.com>
2025-12-17 07:35:04 -0800

    worked technical problem

--- columns.py
+++ columns.py
@@ -1,7 +1,21 @@
 def top_column(matrix):
-  # Write your solution here! 
-  pass
+    rows = len(matrix)
+    col = len(matrix[0])
 
+    max_sum = None
+    max_col_index = 0
+
+    for column in range(col):
+        sum_of_column = 0
+
+        for row in range(rows):
+            sum_of_column += matrix[row][column]
+
+        if max_sum is None or sum_of_column > max_sum:
+            max_sum = sum_of_column
+            max_col_index = column
+
+    return max_col_index        
 
 # Test cases
 matrix_1 = ((5, 6, 7, 8),
@@ -23,3 +37,13 @@ assert top_column(matrix_4) == 1
 
 print("All tests passed!")
 print("Finished early? Discuss time & space complexity")
+
+
+
+# I will make a tuple of each column separately
+
+# using index I will iterate through each column in matrix
+
+# I will add all the numbers which are in that column
+
+# Identifying highest sum - I will compare all colum sum values , get the highest value - return that column index value
